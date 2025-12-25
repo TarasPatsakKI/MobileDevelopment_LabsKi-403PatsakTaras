0b391423e2a32b28396a708380497bae03240f31
TarasPatsakKI <taras.patsak.ki.2022@lpnu.ua>
2025-12-25 18:35:17 +0200

    Create api_client.dart

---
+++ lib/core/api_client.dart
@@ -0,0 +1,23 @@
+import 'package:dio/dio.dart';
+
+class ApiClient {
+  final Dio _dio;
+
+  ApiClient({required String baseUrl}) : _dio = Dio(BaseOptions(baseUrl: baseUrl));
+
+  void setToken(String token) {
+    _dio.options.headers['Authorization'] = 'Bearer $token';
+  }
+
+  void clearToken() {
+    _dio.options.headers.remove('Authorization');
+  }
+
+  Future<Response> get(String path, {Map<String, dynamic>? queryParameters}) async {
+    return _dio.get(path, queryParameters: queryParameters);
+  }
+
+  Future<Response> post(String path, {dynamic data}) async {
+    return _dio.post(path, data: data);
+  }
+}
